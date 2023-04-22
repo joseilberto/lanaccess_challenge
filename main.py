7a7618569f2aa53e1c37cb393eb9f37c00b87652
Jose Fonceca <ilbertofjunior@gmail.com>
2023-04-22 16:04:17 +0200

    Creating main file for training

---
+++ main.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+
+def main() -> None:
+    """
+    This function executes the model selection and training for this project    
+    """
+
+
+if __name__ == "__main__":
+    main()
