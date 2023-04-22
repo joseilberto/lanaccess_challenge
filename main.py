5b23db350475c1515d422295355087ae8141c375
Jose Fonceca <ilbertofjunior@gmail.com>
2023-04-22 19:31:46 +0200

    Updating the main training scrip

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ def main() -> None:
     This function executes the model selection and training for this project
     """
 
-    # Last start using an untrained YOLOv8n model
+    # Last start using a pretrained YOLOv8n model
     # We choose YOLOv8n because it is small and I assume our system specs
     # are fairly limited
     model = YOLO("yolov8n.yaml")
 
     # Here we use MSCOCO dataset with person and motorcycle categories only
     # so we fetch the dataset first
-    data_dir = os.path.join(os.getcwd, "data")
+    data_dir = os.path.join(os.getcwd(), "datasets")
     data_file = get_yolo_data(
-        data_dir, classes=["person", "motorcycle"], sample=True
+        data_dir, classes=["person", "motorcycle"], sample=False
     )
 
     # Here we train the model
@@ -31,7 +31,6 @@ def main() -> None:
         batch=16,
         patience=5,
         save=True,
-        device="gpu",
         name="Two classes COCO with untrained YOLOv8N",
         exist_ok=True,
         verbose=True,
