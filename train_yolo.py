ee397f74ae99ed1c77ff65cb4f91be97d0d03fd1
Jose Fonceca <ilbertofjunior@gmail.com>
2023-04-23 18:50:45 +0200

    Training for 10 epochs

--- train_yolo.py
+++ train_yolo.py
@@ -11,39 +11,39 @@ def main() -> None:
     This function executes the model selection and training for this project
     """
 
-    # Last start using a pretrained YOLOv8n model
-    # We choose YOLOv8n because it is small and I assume our system specs
-    # are fairly limited
-    model = YOLO("yolov8n.yaml")
-
     # Here we use MSCOCO dataset with person and motorcycle categories only
     # so we fetch the dataset first
     data_dir = os.path.join(os.getcwd(), "data")
     data_file = get_yolo_data(
         data_dir,
         classes=["person", "motorcycle"],
-        sample_size=10,
+        sample_size=15000,
         skip_creation=True,
     )
+
+    # Last start using a pretrained YOLOv8n model
+    # We choose YOLOv8n because it is small and I assume our system specs
+    # are fairly limited
+    model = YOLO("yolov8n.pt")
 
     # Here we train the model
     model.train(
         data=data_file,
-        epochs=5,
+        epochs=10,
         imgsz=640,
         batch=16,
         patience=5,
         save=True,
         project="Pretrained YOLOv8N two classes",
-        name="test1",
+        name="model_10_epochs",
         exist_ok=False,
         pretrained=True,
-        optimizer="Adam",
+        optimizer="SGD",
         verbose=True,
         cos_lr=True,
-        lr0=0.01,
-        lrf=0.0001,
-        dropout=0.2,
+        lr0=1e-5,
+        lrf=1e-7,
+        dropout=0.1,
     )
 
 
